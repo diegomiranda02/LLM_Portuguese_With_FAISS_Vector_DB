1623670f1c2fdb2940b61cbf42b85bbe4365a746
diegomiranda02 <diego.miranda02@gmail.com>
2023-06-21 13:49:22 -0300

    Update main_CONSOLE.py

--- main_CONSOLE.py
+++ main_CONSOLE.py
@@ -7,10 +7,12 @@ toolFactory = ToolFactory()
 toolFactory.register_tool(ToolWhoCreateDocumentOfTCUReport().description, ToolWhoCreateDocumentOfTCUReport())
 toolFactory.register_tool(ToolWhoCreateWorkReportHistory().description, ToolWhoCreateWorkReportHistory())
 toolFactory.register_tool(ToolWhoCreateFinancialReport().description, ToolWhoCreateFinancialReport())
+toolFactory.register_tool(ToolWhichCreatesAReportOfTheMostSoldProducts().description, ToolWhichCreatesAReportOfTheMostSoldProducts())
+toolFactory.register_tool(ToolWhichAddNewEmployent().description, ToolWhichAddNewEmployent())
 toolFactory.generate_tools_vector_database()
 
-command = "Gere um relatorio financeiro do mes de abril e no ano de 2023"
-# command = "Gere um relatorio funcional do servidor Diego Miranda de Paula, matricula 12345"
+# command = "Gere um relatorio financeiro do mes de abril e no ano de 2023"
+command = "Cadastre o funcionario Diego Miranda de Paula, CPF 11111111, RG 1234412334, emitida em 02/02/2202 por SSP/RN, residente à Rua Teste de Rua Natal da Silva, numero 1234, apt 500000, torre 199191919, condominio teste teste teste, bairro nova parnamirim, cep 1212122112, no estado do Rio Grande do Norte. Com salario de R$ 400,00 por mês, carga horaria de 40 horas semanais, 8 horas por dia, regime de contratacao CLT."
 
 # Getting the tool to execute the command
 tool = toolFactory.get_tool(command)
