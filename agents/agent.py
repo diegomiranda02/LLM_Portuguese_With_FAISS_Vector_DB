1072d2e920bec5154710d66ef67ace329a4d21fd
diegomiranda02 <diego.miranda02@gmail.com>
2023-06-21 13:51:27 -0300

    Update agent.py

--- agents/agent.py
+++ agents/agent.py
@@ -5,11 +5,8 @@ from interfaces.toolInterfaces import ToolInterface
 
 from models import BERT_portuguese_model_api as LLM_BERT
 
-# import urllib library
-from urllib.request import urlopen
-
-# Import json package to load json from the URL response
-import json
+# import requests library
+import requests
 
 class Agent:
 
@@ -26,17 +23,11 @@ class Agent:
       # Fulfilling each requirements questioning to the LLM
       fulfilled_requirements[requirement] = LLM_BERT.getAnswer(command, requirement)
 
-    #return self.__tool.run(fulfilled_requirements)
-
     url = self.__tool.run(fulfilled_requirements)
 
-    print("AGENT ACCESSING URL: {}".format(url))
-
     # store the response of URL
-    response = urlopen(url)
-  
-    # storing the JSON response from url in data
-    data_json = json.loads(response.read())
+    response = requests.get(url)
+    print(response.json())
   
     # Return the json response
-    return data_json
+    return response.json()
